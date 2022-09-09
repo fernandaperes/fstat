829ec7b2a9570a5b36aeeaeabfc1a7cc43040841
Fernanda Fiel Peres <fernandafperes@hotmail.com>
2022-09-09 19:40:25 -0300

    alpha

--- R/graf_obs_esp_ind.R
+++ R/graf_obs_esp_ind.R
@@ -58,19 +58,19 @@ graf_obs_esp_ind <- function(dados, v1, v2, cat_graf_v2, xlab = "",
     cor <- c(cor, colorspace::lighten(cor, amount = 0.4))
   }else{}
 
+  if(isTRUE(asteriscos)){alpha <- 0}else{alpha <- 1}
 
   g <- banco_graf %>% filter({{v2}} == cat_graf_v2) %>%
     ggplot(aes(x = {{v1}}, y = Valor, fill = Resultado)) +
     geom_bar(position = "dodge", stat = "identity") +
+    geom_text(aes(y = Valor+2, label = rotulos),
+              position = position_dodge(width = 0.9), size = 4.5,
+              alpha = alpha) +
     labs(y = "Frequência (n)", x = xlab, fill = NULL) +
     scale_fill_manual(values = cor) +
     scale_y_continuous(expand = expansion(mult = c(0, 0.05))) +
     theme_classic() +
     theme(legend.position = "bottom")
-
-  if(isTRUE(asteriscos)){
-    g <- g + geom_text(aes(y = Valor+2, label = rotulos),
-                       position = position_dodge(width = 0.9), size = 4.5)}else{}
 
   return(g)
 
